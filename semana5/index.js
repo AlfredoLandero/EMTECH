d1bcef2923a356587400bd014dab794bd38aa607
AlfredoLandero <99086384+AlfredoLandero@users.noreply.github.com>
2023-01-09 18:53:30 -0600

    Add files via upload

---
+++ semana5/index.js
@@ -0,0 +1,24 @@
+const datoa = String("hola");
+console.log(datoa);
+alert (datoa);
+
+const datob = 25;
+console.log(datob);
+alert (datob);
+
+const datoc = true;
+console.log(datoc);
+alert (datoc);
+
+const datod = String ("Manuel");
+const datoe = 20;
+const intrpo = alert(`hola ${datod}, tienes ${datoe} años?`);
+
+const num1 = 40;
+const num2 = 12;
+const resultado = num1 + num2;
+alert(resultado);
+
+if (resultado != 53){
+    alert("resultado incorrecto")
+}
